0d2f30dc4734a427ba6a0849093eaa10a4e7f2a9
Cuongdao2222 <cuongdao12121@gmail.com>
2023-05-18 10:23:22 +0700

    Update apps.blade.php

--- resources/views/frontend/layouts/apps.blade.php
+++ resources/views/frontend/layouts/apps.blade.php
@@ -16,7 +16,7 @@
         <link href="favicon.ico" rel="shortcut icon">
         <meta name="google-site-verification" content="iab7ppCh8CQx4bhanyoz0fN-ol-cDT4LogjVuqdO27o">
         <link rel="stylesheet" type="text/css" href="{{ asset('css/swiper.min.css')}}">
-        <link rel="stylesheet" type="text/css" href="{{ asset('css/style.css')}}">
+        <link rel="stylesheet" type="text/css" href="{{ asset('css/style.css')}}?ver=1">
         <link rel="stylesheet" type="text/css" href="{{ asset('css/grid.css')}}">
         <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css">
         <link rel="preconnect" href="https://fonts.gstatic.com">
