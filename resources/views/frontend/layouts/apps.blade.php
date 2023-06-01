71dac13a4212b31644e6178dec306aed18850e13
Cuongdao2222 <cuongdao12121@gmail.com>
2023-06-01 09:13:49 +0700

    update

--- resources/views/frontend/layouts/apps.blade.php
+++ resources/views/frontend/layouts/apps.blade.php
@@ -27,7 +27,7 @@
         <link href="favicon.ico" rel="shortcut icon">
         <meta name="google-site-verification" content="iab7ppCh8CQx4bhanyoz0fN-ol-cDT4LogjVuqdO27o">
         <link rel="stylesheet" type="text/css" href="{{ asset('css/swiper.min.css')}}">
-        <link rel="stylesheet" type="text/css" href="{{ asset('css/style.css')}}?ver=1">
+        <link rel="stylesheet" type="text/css" href="{{ asset('css/style.css')}}?ver=2">
         <link rel="stylesheet" type="text/css" href="{{ asset('css/grid.css')}}">
         <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css">
         <link rel="preconnect" href="https://fonts.gstatic.com">
@@ -47,7 +47,7 @@
             <div class="flex-container flex-space-between">
                 <div class="cell-3-10">
                     <div class="c5"></div>
-                    <div class="logo"><a href="{{ route('homeFe') }}"><img src="../uploaded/logo/logo%20%281%29.png" alt="LogoSite" style="max-width:100%; margin:0 auto"></a></div>
+                    <!-- <div class="logo"><a href="{{ route('homeFe') }}"><img src="../uploaded/logo/logo%20%281%29.png" alt="LogoSite" style="max-width:100%; margin:0 auto"></a></div> -->
                     <div class="c5"></div>
                 </div>
                 <div class="cell-4-10" style="position:relative;">
