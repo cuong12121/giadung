233b1ceb46f3eb9d806a6f160068b39146ffd412
Cuongdao2222 <cuongdao12121@gmail.com>
2023-06-19 11:28:49 +0700

    Update footer.blade.php

--- resources/views/frontend/layouts/footer.blade.php
+++ resources/views/frontend/layouts/footer.blade.php
@@ -1,56 +1,54 @@
+<style type="text/css">
+    .info-footer{
+        display: flex;
+    }
+
+    .info-footer .title{
+        text-align: center;
+    }
+</style>
+
 <div class="footer">
     <div class="grid">
         <div class="c30"></div>
         <div class="flex-container flex-space-between">
             <div class="cell-2-3  tab-cell-1-1 mobile-cell-1-1">
-                <!-- <div class="flex-container flex-space-between">
-                    <div class="cell-2-3  tab-cell-1-1 mobile-cell-1-1">
-                        <div class="footer-title">
-                            Bear Việt Nam - Đại lý cấp 1 chính thức của hãng gia dụng Bear tại Việt Nam
-                        </div>
-                        <div class="c10" style="border-bottom:solid 1px #666;"></div>
-                        <div class="c15"></div>
-                        <div class="footer-text">
-                            <h4 condensed="" roboto="" style="box-sizing: border-box; text-align: justify;"><strong><span style="line-height:2;"><span style="font-size:18px;"><span style="font-family:arial,helvetica,sans-serif;"><span style="box-sizing: border-box;">TH&Ocirc;NG TIN LI&Ecirc;N HỆ:</span></span></span></span></strong></h4>
-                            <div style="text-align: justify;"><span style="line-height:2;"><span style="font-size:18px;"><span style="font-family:arial,helvetica,sans-serif;"><strong>Website:</strong> <u><strong><a href="index.htm" target="_blank"><span style="color:#FF8C00;">Bearvietnam.com.vn</span></a></strong></u><br>
-                                <strong>Fanpage:&nbsp;<a href="https://www.facebook.com/bearvietnam.com.vn/" target="_blank"><span style="color:#FF8C00;">https://www.facebook.com/bearvietnam.com.vn/</span></a></strong></span></span></span><br>
-                                <span style="line-height:2;"><span style="font-size:18px;"><span style="font-family:arial,helvetica,sans-serif;"><strong>HOTLINE:</strong>&nbsp; 0348 949 926<br>
-                                <strong>Email:</strong> Bearvietnam.com.vn@gmail.com<br>
-                                <strong>Mở cửa:</strong> 8:00 - 23:00<br>
-                                <strong>Địa chỉ:&nbsp;</strong>T4 Thăng Long Capital An Kh&aacute;nh Ho&agrave;i Đức H&agrave; Nội.</span></span></span><br>
-                                <br>
-                                &nbsp;
-                            </div>
-                            <div class="company item " style="box-sizing: border-box; color: rgb(168, 168, 168); list-style: none; margin-bottom: 10px;box-sizing: border-box; margin-bottom: 5px;">&nbsp;</div>
-                        </div>
-                        <div class="c10" style="border-bottom:solid 1px #666;"></div>
-                        <div class="c10"></div>
-                        <div class="linksmo">
-                            <a href="https://www.facebook.com/bearvietnam.com.vn/" class="fa fa-facebook" target="_blank" rel="nofollow"></a>
-                            <a href="https://www.youtube.com/channel/UCQfSswU3l0kBrg0Utg0gWlA/" class="fa fa-youtube" target="_blank" rel="nofollow"></a>
-                            <a href="" class="fa fa-google-plus" target="_blank" rel="nofollow"></a>
-                        </div>
-                    </div>
-                    <div class="cell-1-3  tab-cell-1-1 mobile-cell-1-1" style="text-align: center;">
-                        <div class="flex-container flex-space-between">
-                            <div class="cell-1-2 pad-cell-1-3 tab-cell-1-2 mobile-cell-1-2">
-                            </div>
-                            <div class="cell-1-2 pad-cell-1-3 tab-cell-1-2 mobile-cell-1-2">
-                            </div>
-                            <div class="cell-1-6 pad-cell-1-3 tab-cell-1-2 mobile-cell-1-2">
-                            </div>
-                            <div class="cell-1-6 pad-cell-1-3 tab-cell-1-2 mobile-cell-1-2">
-                            </div>
-                            <div class="cell-1-6 pad-cell-1-3 tab-cell-1-2 mobile-cell-1-2">
-                            </div>
-                            <div class="cell-1-6 pad-cell-1-3 tab-cell-1-2 mobile-cell-1-2">
-                            </div>
-                        </div>
-                    </div>
-                </div> -->
+                <div class="row info-footer">
+                   <div class="col-md-2 col-footer">
+                      <h3 class="title">Thông tin</h3>
+                      <ul class="footer_list-link">
+                         <li> <a href="">Giới thiệu</a> </li>
+                         <li> <a href="">Liên hệ</a> </li>
+                         <li> <a href="">Dự án bán buôn</a> </li>
+                         <li class="mobile"> <a href="/?show=pc_view">Xem bản desktop</a> </li>
+                      </ul>
+                   </div>
+                   <div class="col-md-3 col-footer">
+                      <h3 class="title">Hỗ trợ mua hàng</h3>
+                      <ul class="footer_list-link">
+                         <li> <a href="">Hướng dẫn trả góp</a> </li>
+                         <li> <a href="">Cách thức thanh toán</a> </li>
+                         <li> <a href="">Bảng giá vật tư lắp đặt</a> </li>
+                      </ul>
+                   </div>
+                   <div class="col-md-4 col-footer">
+                      <h3 class="title">Chính sách</h3>
+                      <ul class="footer_list-link">
+                         <li> <a href="">Chính sách &amp; quy định chung</a> </li>
+                         <li> <a href="">Chính sách đổi trả sản phẩm</a> </li>
+                         <li> <a href="">Chính sách bảo hành</a> </li>
+                         <li> <a href="">Chính sách giao hàng</a> </li>
+                      </ul>
+                   </div>
+                   <div class="col-md-3 col-footer">
+                     
+                      <h4 style="margin-top: 20px;margin-bottom: 10px;">Kết nối với chúng tôi</h4>
+                      
+                   </div>
+                </div>
             </div>
             <div class="cell-1-3 tab-cell-1-1 hide-on-tab">
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3724.5595261426015!2d105.71971871529779!3d21.010286586008498!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x313453335cfa80ad%3A0x19e88d2aaac499d8!2sChung%20C%C6%B0%20Th%C4%83ng%20Long%20Capital!5e0!3m2!1sen!2s!4v1667642405272!5m2!1sen!2s" width="100%" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+               <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3724.9385871785303!2d105.89182860249123!3d20.99509873491041!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3135aeb6d3414e2b%3A0x660cb995e3acdb25!2zNjgzIE5ndXnhu4VuIEtob8OhaSwgVGhhbmggVHLDrCwgSGFpIELDoCBUcsawbmcsIEjDoCBO4buZaSwgVmnhu4d0IE5hbQ!5e0!3m2!1svi!2s!4v1687145589324!5m2!1svi!2s" width="500" height="250" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                 <div class="c20"></div>
             </div>
         </div>
