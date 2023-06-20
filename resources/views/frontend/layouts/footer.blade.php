9d8b9efdd961d36dee59fefebbf261468036ff88
Cuongdao2222 <cuongdao12121@gmail.com>
2023-06-20 08:36:39 +0700

    Update footer.blade.php

--- resources/views/frontend/layouts/footer.blade.php
+++ resources/views/frontend/layouts/footer.blade.php
@@ -6,49 +6,110 @@
     .info-footer .title{
         text-align: center;
     }
+
+    .row-full-width{
+        display: flex;
+        font-size: 14px;
+    }
+
+    .cell-1-3{
+        width: 24%;
+    }
+
+    .cell-2-3{
+
+        width: 76%;
+    } 
+
+    .cell-1-1-3{
+        width: 40%;
+    } 
+
+    .cell-1-2-3{
+        width: 20%;
+    }  
+
+    .tab-map{
+        margin-top:60px;
+    }
 </style>
 
 <div class="footer">
-    <div class="grid">
+    <div class="">
         <div class="c30"></div>
-        <div class="flex-container flex-space-between">
+        <div class="">
             <div class="cell-2-3  tab-cell-1-1 mobile-cell-1-1">
-                <div class="row info-footer">
-                   <div class="col-md-2 col-footer">
-                      <h3 class="title">Thông tin</h3>
-                      <ul class="footer_list-link">
-                         <li> <a href="">Giới thiệu</a> </li>
-                         <li> <a href="">Liên hệ</a> </li>
-                         <li> <a href="">Dự án bán buôn</a> </li>
-                         <li class="mobile"> <a href="/?show=pc_view">Xem bản desktop</a> </li>
-                      </ul>
-                   </div>
-                   <div class="col-md-3 col-footer">
-                      <h3 class="title">Hỗ trợ mua hàng</h3>
-                      <ul class="footer_list-link">
-                         <li> <a href="">Hướng dẫn trả góp</a> </li>
-                         <li> <a href="">Cách thức thanh toán</a> </li>
-                         <li> <a href="">Bảng giá vật tư lắp đặt</a> </li>
-                      </ul>
-                   </div>
-                   <div class="col-md-4 col-footer">
-                      <h3 class="title">Chính sách</h3>
-                      <ul class="footer_list-link">
-                         <li> <a href="">Chính sách &amp; quy định chung</a> </li>
-                         <li> <a href="">Chính sách đổi trả sản phẩm</a> </li>
-                         <li> <a href="">Chính sách bảo hành</a> </li>
-                         <li> <a href="">Chính sách giao hàng</a> </li>
-                      </ul>
-                   </div>
-                   <div class="col-md-3 col-footer">
-                     
-                      <h4 style="margin-top: 20px;margin-bottom: 10px;">Kết nối với chúng tôi</h4>
-                      
-                   </div>
+                <div class="customfooter" style="padding-top: 50px; padding-bottom: 30px; padding-left: 35px;">
+                    <div class="row row-small dark row-full-width">
+                        <div class="cell-1-1-3 medium-12 small-12 col">
+                            <!-- <p class="title">Bear Việt Nam</p> -->
+                            <a href="https://www.facebook.com/bearvietnam.vn" class="social" target="_blank">
+                                <img src="/wp-content/themes/shopping/images/fb.png" alt="Facebook" data-lazy-src="/wp-content/themes/shopping/images/fb.png" data-ll-status="loaded" class="entered lazyloaded">
+                                <noscript><img src="/wp-content/themes/shopping/images/fb.png" alt="Facebook"></noscript>
+                            </a>
+                            <a href="#" class="social" target="_blank">
+                                <img src="/wp-content/themes/shopping/images/twitter.png" alt="Twitter" data-lazy-src="/wp-content/themes/shopping/images/twitter.png" data-ll-status="loaded" class="entered lazyloaded">
+                                <noscript><img src="/wp-content/themes/shopping/images/twitter.png" alt="Twitter"></noscript>
+                            </a>
+                            <a href="#" class="social" target="_blank">
+                                <img src="/wp-content/themes/shopping/images/insta.png" alt="Instagram" data-lazy-src="/wp-content/themes/shopping/images/insta.png" data-ll-status="loaded" class="entered lazyloaded">
+                                <noscript><img src="/wp-content/themes/shopping/images/insta.png" alt="Instagram"></noscript>
+                            </a>
+                            <a href="https://www.youtube.com/@BearVietnam" class="social" target="_blank">
+                                <img src="/wp-content/themes/shopping/images/youtube.png" alt="Youtube" data-lazy-src="/wp-content/themes/shopping/images/youtube.png" data-ll-status="loaded" class="entered lazyloaded">
+                                <noscript><img src="/wp-content/themes/shopping/images/youtube.png" alt="Youtube"></noscript>
+                            </a>
+                            <p><i class="fa fa-map-marker"></i>19 Nguyễn Trãi, Thanh Xuân, Hà Nội</p>
+                            <p><i class="fa fa-phone"></i>1800.6161</p>
+                            <p><i class="fa fa-envelope"></i>cskh@bearvietnam.vn</p>
+                            <p>MST: 0108085048 – Ngày cấp: 06/12/2017</p>
+                            <p>Nơi cấp: sở Kế hoạch và đầu tư TP. Hà Nội</p>
+                            <p>Đại Diện: Ông Lê Anh Tuấn – Chức vụ: Tổng Giám Đốc</p>
+                        </div>
+                        <div class="cell-1-1-3 medium-6 small-12 col">
+                            <p class="title">Danh mục sản phẩm</p>
+                            <p><a href="#">Nồi điện thông minh</a></p>
+                            <p><a href="#">Máy xay đa năng</a></p>
+                            <p><a href="#">Thiết bị làm bánh</a></p>
+                            <p><a href="#">Gia dụng nhà bếp khác</a></p>
+                        </div>
+                        <div class="cell-1-2-3 medium-6 small-12 col">
+                            <p class="title">Chính sách chung</p>
+                            <p><a href="#">Chính sách bảo mật</a></p>
+                            <p><a href="#">Chính sách giao hàng</a></p>
+                            <p><a href="#">Chính sách bảo hành</a></p>
+                            <p><a href="#">Chính sách thanh toán</a></p>
+                           
+                        </div>
+                      <!--   <div class="large-2 medium-12 small-12 col">
+                            <p class="title">Kết nối với chúng tôi</p>
+                            <a href="https://www.facebook.com/bearvietnam.vn" class="social" target="_blank">
+                                <img src="/wp-content/themes/shopping/images/fb.png" alt="Facebook" data-lazy-src="/wp-content/themes/shopping/images/fb.png" data-ll-status="loaded" class="entered lazyloaded">
+                                <noscript><img src="/wp-content/themes/shopping/images/fb.png" alt="Facebook"></noscript>
+                            </a>
+                            <a href="#" class="social" target="_blank">
+                                <img src="/wp-content/themes/shopping/images/twitter.png" alt="Twitter" data-lazy-src="/wp-content/themes/shopping/images/twitter.png" data-ll-status="loaded" class="entered lazyloaded">
+                                <noscript><img src="/wp-content/themes/shopping/images/twitter.png" alt="Twitter"></noscript>
+                            </a>
+                            <a href="#" class="social" target="_blank">
+                                <img src="/wp-content/themes/shopping/images/insta.png" alt="Instagram" data-lazy-src="/wp-content/themes/shopping/images/insta.png" data-ll-status="loaded" class="entered lazyloaded">
+                                <noscript><img src="/wp-content/themes/shopping/images/insta.png" alt="Instagram"></noscript>
+                            </a>
+                            <a href="https://www.youtube.com/@BearVietnam" class="social" target="_blank">
+                                <img src="/wp-content/themes/shopping/images/youtube.png" alt="Youtube" data-lazy-src="/wp-content/themes/shopping/images/youtube.png" data-ll-status="loaded" class="entered lazyloaded">
+                                <noscript><img src="/wp-content/themes/shopping/images/youtube.png" alt="Youtube"></noscript>
+                            </a>
+                            <p><strong>Bảo hành sản phẩm</strong></p>
+                            <p>
+                                <a href="https://bearvietnam.vn/bao-hanh">
+                                    <img decoding="async" class="aligncenter size-full wp-image-6655 entered lazyloaded" src="https://bearvietnam.vn/wp-content/uploads/baohanhqr.png.webp" alt="" width="265" height="265" data-lazy-srcset="https://bearvietnam.vn/wp-content/uploads/baohanhqr.png.webp 265w,https://bearvietnam.vn/wp-content/uploads/baohanhqr-100x100.png.webp 100w" data-lazy-sizes="(max-width: 265px) 100vw, 265px" data-lazy-src="https://bearvietnam.vn/wp-content/uploads/baohanhqr.png.webp" data-ll-status="loaded" sizes="(max-width: 265px) 100vw, 265px" srcset="https://bearvietnam.vn/wp-content/uploads/baohanhqr.png.webp 265w,https://bearvietnam.vn/wp-content/uploads/baohanhqr-100x100.png.webp 100w">
+                            <noscript><img decoding="async" class="aligncenter size-full wp-image-6655" src="https://bearvietnam.vn/wp-content/uploads/baohanhqr.png.webp" alt="" width="265" height="265" srcset="https://bearvietnam.vn/wp-content/uploads/baohanhqr.png.webp 265w,https://bearvietnam.vn/wp-content/uploads/baohanhqr-100x100.png.webp 100w" sizes="(max-width: 265px) 100vw, 265px" /></noscript></a></p>
+                        </div> -->
+                    </div>
                 </div>
             </div>
-            <div class="cell-1-3 tab-cell-1-1 hide-on-tab">
-               <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3724.9385871785303!2d105.89182860249123!3d20.99509873491041!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3135aeb6d3414e2b%3A0x660cb995e3acdb25!2zNjgzIE5ndXnhu4VuIEtob8OhaSwgVGhhbmggVHLDrCwgSGFpIELDoCBUcsawbmcsIEjDoCBO4buZaSwgVmnhu4d0IE5hbQ!5e0!3m2!1svi!2s!4v1687145589324!5m2!1svi!2s" width="500" height="250" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            <div class="cell-1-3 tab-cell-1-1 hide-on-tab tab-map">
+               <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3724.9385871785303!2d105.89182860249123!3d20.99509873491041!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3135aeb6d3414e2b%3A0x660cb995e3acdb25!2zNjgzIE5ndXnhu4VuIEtob8OhaSwgVGhhbmggVHLDrCwgSGFpIELDoCBUcsawbmcsIEjDoCBO4buZaSwgVmnhu4d0IE5hbQ!5e0!3m2!1svi!2s!4v1687145589324!5m2!1svi!2s" width="300" height="250" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                 <div class="c20"></div>
             </div>
         </div>
