bca7ffbb0ff976292b7e7ec27f846deed543800e
Cuongdao2222 <cuongdao12121@gmail.com>
2023-10-10 15:32:18 +0700

    Update blog.blade.php

--- resources/views/frontend/blog.blade.php
+++ resources/views/frontend/blog.blade.php
@@ -10,10 +10,10 @@
     <link rel="stylesheet" href="{{ asset('css/customs.css') }}?v=245754.75.52928">
     @endpush
    
-     <link rel="stylesheet" type="text/css" href="{{ asset('css/category.css') }}"> 
+     <!-- <link rel="stylesheet" type="text/css" href="{{ asset('css/category.css') }}">  -->
 
         <link rel="stylesheet" type="text/css" href="{{ asset('css/categories.css') }}?ver=1"> 
-         <link rel="stylesheet" type="text/css" href="{{ asset('css/dienmay.css') }}?ver=1"> 
+         <!-- <link rel="stylesheet" type="text/css" href="{{ asset('css/dienmay.css') }}?ver=1">  -->
     <style type="text/css">
         .header__top-mobile{
             height: 133px;
