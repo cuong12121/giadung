44dbacedb83b82af1658142baefad5b58d541a14
Cuongdao2222 <cuongdao12121@gmail.com>
2023-11-04 15:50:43 +0700

    Update edit.blade.php

--- resources/views/products/edit.blade.php
+++ resources/views/products/edit.blade.php
@@ -121,7 +121,7 @@
 
         //level phần tử cha gần nhất bằng phần tử con trừ 1, sau đó lấy list sản phẩm của phần tử cha nó
 
-        $level_parent = $level_child-1;
+        $level_parent = $level_child-2;
 
         $parent = App\Models\groupProduct::select('parent_id', 'product_id')->where('level',$level_parent)->get();
 
