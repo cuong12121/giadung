ab53833c2579e661e240f5f64d608a0005f2920d
Cuongdao2222 <cuongdao12121@gmail.com>
2023-11-10 09:24:58 +0700

    Update apps.blade.php

--- resources/views/frontend/layouts/apps.blade.php
+++ resources/views/frontend/layouts/apps.blade.php
@@ -29,7 +29,7 @@
        
 
         <link rel="stylesheet" type="text/css" href="{{ asset('css/swiper.min.css')}}">
-        <link rel="stylesheet" type="text/css" href="{{ asset('css/style.css')}}?ver=13">
+        <link rel="stylesheet" type="text/css" href="{{ asset('css/style.css')}}?ver=14">
         <link rel="stylesheet" type="text/css" href="{{ asset('css/grid.css')}}?ver=1">
         <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css">
         <link rel="preconnect" href="https://fonts.gstatic.com">
